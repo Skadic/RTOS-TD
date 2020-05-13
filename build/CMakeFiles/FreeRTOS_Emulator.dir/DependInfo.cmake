189d8ff8605299daa253da0bdb62e6268714a68f
Skadic <etieti22@gmail.com>
2020-05-13 18:15:40 +0200

    Initial Commit

---
+++ build/CMakeFiles/FreeRTOS_Emulator.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/AsyncIO/AsyncIO.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/AsyncIO/AsyncIO.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/FreeRTOS_Kernel/croutine.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/croutine.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/FreeRTOS_Kernel/list.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/list.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/FreeRTOS_Kernel/portable/GCC/Posix/port.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/portable/GCC/Posix/port.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/FreeRTOS_Kernel/portable/MemMang/heap_3.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/portable/MemMang/heap_3.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/FreeRTOS_Kernel/queue.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/queue.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/FreeRTOS_Kernel/tasks.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/tasks.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/FreeRTOS_Kernel/timers.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/timers.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/Gfx/TUM_Ball.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Ball.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/Gfx/TUM_Draw.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Draw.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/Gfx/TUM_Event.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Event.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/Gfx/TUM_Font.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Font.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/Gfx/TUM_Sound.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Sound.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/lib/Gfx/TUM_Utils.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Utils.c.o"
+  "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/src/main.c" "/home/skadic/Dokumente/Uni/FP/FreeRTOS-Emulator-master/build/CMakeFiles/FreeRTOS_Emulator.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "../lib/FreeRTOS_Kernel/include"
+  "../lib/FreeRTOS_Kernel/portable/GCC/Posix"
+  "../include"
+  "../lib/Gfx/include"
+  "../lib/AsyncIO/include"
+  "../lib/tracer/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("Hey");
+}
+
+
+// Emulator doesn't compile if these aren't here
+__attribute__((unused)) void vMainQueueSendPassed(void)
+{
+    /* This is just an example implementation of the "queue send" trace hook. */
+}
+
+__attribute__((unused)) void vApplicationIdleHook(void)
+{
+
+}
