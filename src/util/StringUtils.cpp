99dda14a7159b1a7809b38f48cbc76fa17faca93
hendrik6687 <hendrik.weissenfels@gmx.de>
2020-07-17 00:19:50 +0200

    wave implementierung - es hat funktioneirt!! StringUtils.cpp und StringUtils.h hinzugefügt

---
+++ src/util/StringUtils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hendrik on 16.07.20.
+//
+
+#include "StringUtils.h"
---
+++ src/util/StringUtils.h
@@ -0,0 +1,20 @@
+//
+// Created by hendrik on 16.07.20.
+//
+
+#pragma once
+
+#include <sstream>
+#include <TUM_Draw.h>
+
+template<class T>
+
+void drawInfo(char* text, T number, int x, int y){
+    std::stringstream ss;
+    ss << std::noskipws << text;
+    ss << number;
+    std::string s;
+    std::getline(ss, s);
+    const char *chars = s.c_str();
+    tumDrawText(const_cast<char *>(chars), x, y, 0xFFFFFF);
+}
